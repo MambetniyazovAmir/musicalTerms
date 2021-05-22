b9ee507efed62fbf8c2f8146b8c8e2e5ce209fcc
temurz <timurzaitov1998@gmail.com>
2021-05-22 12:51:36 +0500

    clicker

--- app/src/main/java/com/musicalTerms/MainActivity.kt
+++ app/src/main/java/com/musicalTerms/MainActivity.kt
@@ -2,10 +2,16 @@ package com.musicalTerms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(R.id.fragment1,MainFragment()).commit()
+        imgCross.setOnClickListener{
+            imgCross.setBackgroundResource(R.drawable.ic_baseline_close_24)
+        }
     }
 }
