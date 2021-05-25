9b6e51e54c0375b3f65cb99a881adec3f8e85206
temurz <timurzaitov1998@gmail.com>
2021-05-25 19:48:34 +0500

    search editted

--- app/src/main/java/com/musicalTerms/MainFragment.kt
+++ app/src/main/java/com/musicalTerms/MainFragment.kt
@@ -1,17 +1,13 @@
 package com.musicalTerms
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment(R.layout.main_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        imgCross.setOnClickListener{
-            imgCross.setImageResource(R.drawable.ic_baseline_close_24)
-        }
+
     }
 }
